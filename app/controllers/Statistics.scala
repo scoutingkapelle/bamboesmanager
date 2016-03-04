aac1a94658668dd0f28b901cffa632b760df79cf
Willem Jan Glerum <willem-jan.glerum@lunatech.com>
2016-03-04 21:51:42 +0100

    Use for-comprehension for statistics

--- app/controllers/Statistics.scala
+++ app/controllers/Statistics.scala
@@ -10,7 +10,6 @@ import play.api.i18n.{Messages, MessagesApi}
 import play.api.libs.json.Json
 
 import scala.concurrent.ExecutionContext.Implicits._
-import scala.concurrent.Future
 
 class Statistics @Inject()(statisticsDAO: StatisticsDAO,
                            val messagesApi: MessagesApi,
@@ -18,22 +17,20 @@ class Statistics @Inject()(statisticsDAO: StatisticsDAO,
   extends Silhouette[User, SessionAuthenticator] {
 
   def statistics = SecuredAction.async { implicit request =>
-    statisticsDAO.friday.flatMap(friday =>
-      statisticsDAO.saturday.flatMap(saturday =>
-        statisticsDAO.sorting.flatMap(sorting =>
-          statisticsDAO.selling.flatMap(selling => {
-            val statistics = Map(
-              Messages("friday") -> friday.sortBy(_._1).toMap,
-              Messages("saturday") -> saturday.sortBy(_._1).toMap,
-              Messages("sorting") -> sorting.sortBy(_._1).toMap,
-              Messages("selling") -> selling.sortBy(_._1).toMap
-            )
-            Future.successful(Ok(views.html.statistics(statistics, request.identity)))
-          }
-          )
-        )
+    for {
+      friday <- statisticsDAO.friday
+      saturday <- statisticsDAO.saturday
+      sorting <- statisticsDAO.sorting
+      selling <- statisticsDAO.selling
+    } yield {
+      val statistics = Map(
+        Messages("friday") -> friday.sortBy(_._1).toMap,
+        Messages("saturday") -> saturday.sortBy(_._1).toMap,
+        Messages("sorting") -> sorting.sortBy(_._1).toMap,
+        Messages("selling") -> selling.sortBy(_._1).toMap
       )
-    )
+      Ok(views.html.statistics(statistics, request.identity))
+    }
   }
 
   def friday = SecuredAction.async { implicit request =>
