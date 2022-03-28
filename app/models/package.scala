f83255a35f1b894569987fe5744eb3e7841c82f7
Willem Jan Glerum <wjglerum@gmail.com>
2022-03-28 10:59:53 +0200

    Organise and cleanup models

--- app/models/package.scala
+++ app/models/package.scala
@@ -1,6 +1,21 @@
+import com.mohiva.play.silhouette.api.Identity
 import play.api.libs.json.{Json, Writes}
 
+import java.util.UUID
+
 package object models {
+
+  case class Person(id: UUID, name: String, email: String, age: Int, group: Group)
+
+  case class Organisation(id: UUID, name: String)
+
+  case class Group(id: UUID, name: String, organisation: Organisation)
+
+  case class Category(id: UUID, name: String)
+
+  case class Registration(id: UUID, person: Person, friday: Boolean, saturday: Boolean, sorting: Boolean, category: Option[Category], teamLeader: Boolean)
+
+  case class User(id: UUID, name: String, email: String) extends Identity
 
   implicit val organisationWrites: Writes[Organisation] = Json.writes[Organisation]
   implicit val groupWrites: Writes[Group] = Json.writes[Group]
@@ -18,9 +33,4 @@ package object models {
 
   def organisationsTupled(organisations: Seq[Organisation]): Seq[(String, String)] =
     organisations.sortBy(_.name).map(organisation => (organisation.id.toString, organisation.name))
-
-  def fullName(firstName: String, prefix: String, surName: String): String =
-    (firstName.toLowerCase.split(" ").map(_.capitalize).mkString(" ") + " " +
-      prefix.toLowerCase + " " +
-      surName.toLowerCase.split(" ").map(_.capitalize).mkString(" ")).trim.replaceAll("( )+", " ")
 }
